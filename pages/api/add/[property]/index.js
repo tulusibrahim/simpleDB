8cf23f6f70218baa79a4ed1923a549797044d0c9
tulusibrahim <tulusibrahim20@gmail.com>
2022-06-10 13:37:59 +0700

    feat: add one more checking if token exist in DB

--- pages/api/add/[property]/index.js
+++ pages/api/add/[property]/index.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
     });
 
     let dbProperty = process.env.DB_PROPERTY
+    let dbToken = process.env.DB_TOKEN
     let property = req.query.property
     let id = req.headers.userid
 
@@ -17,21 +18,28 @@ export default async function handler(req, res) {
         res.status(401).json({ message: 'Unauthorized' })
     }
     else {
-        let checkProperty = await supabase.from(dbProperty).select().match({ userId: id, property: property })
+        let checkToken = await supabase.from(dbToken).select().match({ token: id })
 
-        if (checkProperty.data.length > 0) {
-            res.status(400).json({ message: `${property} has been defined. Please try another name.` })
-        }
-        else {
-            let insert = await supabase.from(dbProperty).insert({ userId: id, property: property })
-            console.log(insert)
+        if (checkToken.data.length > 0) {
+            let checkProperty = await supabase.from(dbProperty).select().match({ userId: id, property: property })
 
-            if (insert.error) {
-                res.status(insert.status).json({ message: 'Failed to add new property. Please try again' })
+            if (checkProperty.data.length > 0) {
+                res.status(400).json({ message: `${property} has been defined. Please try another name.` })
             }
             else {
-                res.status(insert.status).json(insert)
+                let insert = await supabase.from(dbProperty).insert({ userId: id, property: property })
+                console.log(insert)
+
+                if (insert.error) {
+                    res.status(insert.status).json({ message: 'Failed to add new property. Please try again' })
+                }
+                else {
+                    res.status(insert.status).json(insert)
+                }
             }
+        }
+        else {
+            res.status(403).json({ message: 'No match token found.' })
         }
     }
 
--- pages/api/newtoken.js
+++ pages/api/newtoken.js
@@ -1,4 +1,5 @@
 import NextCors from 'nextjs-cors';
+import { supabase } from '../../helper/helper';
 
 export default async function handler(req, res) {
     await NextCors(req, res, {
@@ -8,6 +9,11 @@ export default async function handler(req, res) {
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
+    let dbToken = process.env.DB_TOKEN
     let token = Math.random().toString(36).substr(2, 6)
+
+    let select = await supabase.from(dbToken).insert({ token: token })
+    console.log(select)
+
     res.json({ token: token })
 }
