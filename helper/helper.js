8e729586678ae9ca4076564de4a30470e25ad283
tulusibrahim <tulusibrahim20@gmail.com>
2022-06-23 13:58:51 +0700

    fix: update initialization

--- helper/helper.js
+++ helper/helper.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database 
-export const supabase = createClient('https://bbgnpwbarxehpmmnyfgq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzE4NzU2NywiZXhwIjoxOTQyNzYzNTY3fQ.HtwezwLxcXQEMTOl-vH6b9SszRtByhX1FSF-dOEqrBc')
+let url = process.env.NEXT_PUBLIC_SUPABASE_URL
+let key = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+export const supabase = createClient(url, key)
